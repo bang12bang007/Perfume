58550e2fe6f859007221ea3af266058d88e29f2e
bang12bang007 <vongoctrungquan2003@gmail.com>
2023-04-23 15:05:48 +0700

    about

--- acsset/js/slick_about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.slick_item ').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
-  });
-      
